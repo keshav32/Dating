eff4748567a01d860498678ea0538a8ef2d2e35b
keshav7073 <Bahetikeshav32@gmail.com>
2024-08-01 01:18:44 +0530

    First Commit

---
+++ API/Data/migration/20240730201204_initialCreation.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.data.migration
+{
+    /// <inheritdoc />
+    public partial class initialCreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Username = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,7 @@
+namespace API.Entities;
+
+public class AppUser{
+
+ public int id { get; set; }
+ public required string Username { get; set; }   
+}
